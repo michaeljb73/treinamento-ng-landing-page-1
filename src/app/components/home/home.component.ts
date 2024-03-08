d1d0bff7c2dff68c2471d9b4f78ca4733576303c
michaeljb73 <michaeljb73@gmail.com>
2024-03-08 12:50:01 -0300

    Atualizações no home.component e carregamento de imagens para a pasta assets

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
 import { HeaderComponent } from '../header/header.component';
+import { NgOptimizedImage } from '@angular/common';
 
 @Component({
   selector: 'app-home',
   standalone: true,
   imports: [
-    HeaderComponent
+    HeaderComponent,
+    NgOptimizedImage
   ],
   providers: [],
   templateUrl: './home.component.html',
